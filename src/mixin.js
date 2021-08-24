810b02977bf58a38b8f3a1820b24c8893a37f601
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2021-08-24 17:37:33 +0200

    fix: mixin typings

--- src/mixin.js
+++ src/mixin.js
@@ -2,62 +2,69 @@ import { Context } from './Context.js';
 import { windowManager } from './Manager.js';
 
 /**
+ * @typedef {{ context: Context; onFocusEnter(): void; onFocusExit(): void; onFocusDismiss(): ReturnType<import('./Context.js').DismissFunction> }} FocusTrapMixin
+ */
+
+/**
  * Enable focus trap context for components.
- * @template {import('@chialab/dna').ComponentConstructor} T
- * @param {T} superClass The base component class.
+ * @template {import('@chialab/dna').ComponentInstance} T
+ * @param {import('@chialab/dna').ComponentConstructor<T>} superClass The base component class.
  * @param {import('./Context.js').ContextOptions} [options] Focus trap options.
  * @return An extended constructor.
  */
-export const focusTrapMixin = (superClass, options) => class FocusTrapElement extends (/** @type {import('@chialab/dna').ComponentConstructor} */ (superClass)) {
-    /**
-     * @inheritdoc
-     * @param {any[]} args
-     */
-    constructor(...args) {
-        super(...args);
+export const focusTrapMixin = (superClass, options) => {
+    const FocusTrapElement = class FocusTrapElement extends (/** @type {import('@chialab/dna').ComponentConstructor} */ (superClass)) {
         /**
-         * The keyboard navigation context.
-         * @readonly
+         * @inheritdoc
          */
-        this.context = new Context(this, {
-            dismiss: this.onFocusDismiss.bind(this),
-            ...options,
-        });
-        this.addEventListener('focusenter', this.onFocusEnter);
-        this.addEventListener('focusexit', this.onFocusExit);
-    }
+        constructor(...args) {
+            super(...args);
+            /**
+             * The keyboard navigation context.
+             * @readonly
+             */
+            this.context = new Context(this, {
+                dismiss: this.onFocusDismiss.bind(this),
+                ...options,
+            });
+            this.addEventListener('focusenter', this.onFocusEnter);
+            this.addEventListener('focusexit', this.onFocusExit);
+        }
 
-    /**
-     * @inheritdoc
-     */
-    connectedCallback() {
-        super.disconnectedCallback();
-        windowManager.addContext(this.context);
-    }
+        /**
+         * @inheritdoc
+         */
+        connectedCallback() {
+            super.disconnectedCallback();
+            windowManager.addContext(this.context);
+        }
 
-    /**
-     * @inheritdoc
-     */
-    disconnectedCallback() {
-        super.disconnectedCallback();
-        windowManager.removeContext(this.context);
-    }
+        /**
+         * @inheritdoc
+         */
+        disconnectedCallback() {
+            super.disconnectedCallback();
+            windowManager.removeContext(this.context);
+        }
 
-    /**
-     * Callback invoked on focus enter.
-     */
-    onFocusEnter() {}
+        /**
+         * Callback invoked on focus enter.
+         */
+        onFocusEnter() { }
 
-    /**
-     * Callback invoked on focus exit.
-     */
-    onFocusExit() { }
+        /**
+         * Callback invoked on focus exit.
+         */
+        onFocusExit() { }
 
-    /**
-     * Callback invoked on focus dismiss.
-     * @return {ReturnType<import('./Context.js').DismissFunction>}
-     */
-    onFocusDismiss() {
-        return true;
-    }
+        /**
+         * Callback invoked on focus dismiss.
+         * @return {ReturnType<import('./Context.js').DismissFunction>}
+         */
+        onFocusDismiss() {
+            return true;
+        }
+    };
+
+    return /** @type {import('@chialab/dna').ComponentConstructor<T & FocusTrapMixin>} */ (/** @type {unknown} */(FocusTrapElement));
 };
